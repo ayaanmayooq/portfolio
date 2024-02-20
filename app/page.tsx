531c16432112016aea220f910698b961d0b6073c
Ayaan <ayaanmayooq@gmail.com>
2024-02-20 02:03:01 -0500

    skeletal about me

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import "../global.css";
 
 export default function Page() {
     return (
-        <section>
-            <p className="animate-move-up-fade-in text-2xl font-bold">AYAAN</p>
+        <section className="animate-move-up-fade-in">
+            <p>Hi, my name is</p>
+            <div className="py-1">
+                <p className=" text-2xl font-bold">Ayaan</p>
+            </div>
+            <p>I am a senior year student at&nbsp;
+                <a href="https://msu.edu/" className="link link-underline link-underline-black text-[#18453B] text-xl font-bold">Michigan State University</a>
+                 &nbsp;pursuing my BS/MS in Computer Science.</p>
         </section>
     );
 }
