f2befa493fc3bf5157268c4b267f0306b735381f
Ayaan <ayaanmayooq@gmail.com>
2024-02-19 22:06:25 -0500

    better looking work vertical tabs

--- app/components/verticalTabs.tsx
+++ app/components/verticalTabs.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { GeistSans } from 'geist/font/sans';
-
+import "../../global.css"
 
 export function VerticalTabs() {
     const [activeTab, setActiveTab] = useState(1);
@@ -13,56 +13,105 @@ export function VerticalTabs() {
         <div className="container relative flex">
             <div className="tabs flex flex-col">
                 <a
-                    className={`tab px-10 flex-1 py-2 text-center border-l-2 cursor-pointer ${activeTab === 1 ? 'border-l-2 border-gray-500' : ''}`}
+                    className={`tab px-10 flex-1 py-2 border-l-2 cursor-pointer ${activeTab === 1 ? 'border-l-2 border-gray-500' : ''}`}
                     href="#"
                     data-tab="1"
                     onClick={() => handleTabClick(1)}
                 >
-                    Tab 1
+                    Michigan State University
                 </a>
                 <a
-                    className={`tab px-10 flex-1 py-2 text-center border-l-2 cursor-pointer ${activeTab === 2 ? 'border-l-2 border-gray-500' : ''
+                    className={`tab px-10 flex-1 py-2 border-l-2 cursor-pointer ${activeTab === 2 ? 'border-l-2 border-gray-500' : ''
                         }`}
                     href="#"
                     data-tab="2"
                     onClick={() => handleTabClick(2)}
                 >
-                    Tab 2
+                    Vectorform
                 </a>
                 <a
-                    className={`tab px-10 flex-1 py-2 text-center border-l-2 cursor-pointer ${activeTab === 3 ? 'border-l-2 border-gray-500' : ''
+                    className={`tab px-10 flex-1 py-2 border-l-2 cursor-pointer ${activeTab === 3 ? 'border-l-2 border-gray-500' : ''
                         }`}
                     href="#"
                     data-tab="3"
                     onClick={() => handleTabClick(3)}
                 >
-                    Tab 3
+                    Roosevelt Innovations
                 </a>
             </div>
 
-            <div className="flex bg-red-200 ml-4">
+            <div className="flex pl-4">
                 <div
-                    className={`absolute opacity-0 transition-opacity ${activeTab === 1 ? 'opacity-100' : ''
+                    className={`absolute py-2 transition-opacity ${activeTab === 1 ? 'opacity-100 z-10' : 'opacity-0'
                         }`}
                     data-tab="1"
                 >
-                    <p>tab 1 contenasdasdasdassdfgsdfg sg sdf gsdf gsdf gsdfsfgjsdfg snfk gbsdfjkgbsdjkhfbgkjsdhfb gkjsdhbskdbgfdh t</p>
+                    <div>
+                        <div className="mb-6">
+                            <h3 className="text-lg font-semibold">Teaching Assitant</h3>
+                            <p className="text-gray-600">
+                                <a href="https://msu.edu/" target="_blank" className="text-xl font-bold">
+                                    <span className="link link-underline link-underline-black text-black">
+                                        MSU
+                                    </span>
+                                </a> | Aug 2022 - Present</p>
+                            <ul className="list-disc pl-6 mt-2">
+                                <li>Courses: Computer Vision, Computer Networks, Matrix Algebra with Computational Applications.</li>
+                                <li>Guided academic success of students by conducting one-on-one mentorship sessions and collaborated with professors and
+                                    TAs to enhance the course experience.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
 
                 <div
-                    className={`absolute opacity-0 transition-opacity ${activeTab === 2 ? 'opacity-100' : ''
+                    className={`absolute py-2 transition-opacity ${activeTab === 2 ? 'opacity-100 z-10' : 'opacity-0'
                         }`}
                     data-tab="2"
                 >
-                    <p className=''>tab 2 bs</p>
+                    <div>
+                        <div className="mb-6">
+                            <h3 className="text-lg font-semibold">MSU Capstone Software Developer</h3>
+                            <p className="text-gray-600">
+                                <a href="https://www.vectorform.com/" target="_blank" className="text-xl font-bold">
+                                    <span className="link link-underline link-underline-black text-black">
+                                        Vectorform
+                                    </span>
+                                </a> | Jan 2023 - May 2023</p>
+                            <ul className="list-disc pl-6 mt-2">
+                                <li>Developed a Unity-based VR application that provides an innovative virtual training space for AI-enhanced training.</li>
+                                <li>Fine-tuned GPT-3 Davinci via Python on 300 conversations and optimized token utilization for context-driven responses,
+                                    resulting in savings of 1000 tokens per conversation and a 4-second improvement in response times.</li>
+                                <li>Engineered a web application for training replay utilizing Angular, Node.js, and Azure SQL and blob storage.</li>
+                                <li>Integrated an embedded interactable WebGL build of a free-cam training replay system.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
 
                 <div
-                    className={`absolute opacity-0 transition-opacity ${activeTab === 3 ? 'opacity-100' : ''
+                    className={`absolute py-2 transition-opacity ${activeTab === 3 ? 'opacity-100 z-10' : 'opacity-0'
                         }`}
                     data-tab="3"
                 >
-                    <p>pls work</p>
+                    <div>
+                        <div className="mb-6">
+                            <h3 className="text-lg font-semibold">Software Developer Intern</h3>
+                            <p className="text-gray-600">
+                                <a href="https://rooseveltinnovations.com/" target="_blank" className="text-xl font-bold leading-tight">
+                                    <span className="link link-underline link-underline-black text-black">
+                                        Roosevelt Innovations
+                                    </span>
+                                </a> | May 2022 - Aug 2022</p>
+                            <ul className="list-disc pl-6 mt-2">
+                                <li>Implemented an enterprise-level feature (using MEAN stack REST, JSON, Kafka, and containerization) that leverages
+                                    clientspecific information and quotes to accurately recommend insurance packages.</li>
+                                <li>Collaborated closely with underwriting and claims analysis experts to gain insights into claims handling processes.</li>
+                                <li>Utilized concepts of Machine Learning, AI, NLP, word embeddings, and Word2Vec to generate 94% of existing business
+                                    insurance rules with 99% precision.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
