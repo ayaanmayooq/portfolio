6d50743ba2e9a5706fce284c6c111f3bf49f23d6
Ayaan <ayaanmayooq@gmail.com>
2024-02-21 21:13:14 -0500

    masonry layout projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,28 @@
 import { GeistSans } from 'geist/font/sans';
 import { ProjItem } from '@/app/components/projectItem';
 
+const images = [
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg',
+];
 export default function Page() {
     return (
-        <div className={`${GeistSans.className} animate-fade-in`}>
-
-            <ProjItem />
+        <div className=" columns-3 gap-8 space-y-8">
+            {images.map((imageUrl, index) => (
+                <div key={index} className="h-auto max-w-full">
+                    <img src={imageUrl} className=" rounded-lg" alt={`Image ${index + 1}`} />
+                </div>
+            ))}
         </div>
     );
 }
