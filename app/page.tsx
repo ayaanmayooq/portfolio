b42544a8f2128340ebd8db4900829475d38c9cfc
Ayaan Mayooq <shaikaya@msu.edu>
2024-05-02 10:29:25 -0400

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
                 <p>Senior year student at&nbsp;
                     <a href="https://msu.edu/" target="_blank" className="link link-underline link-underline-black text-[#18453B] text-xl font-bold">Michigan State University</a>
                     &nbsp;pursuing BS/MS in Computer Science.</p>
-                <p>Always looking for opportunites to put my knowledge and skills to practical use.</p>
+                <p>Always looking for oppotunities to put my knowledge and skills to practical use.</p>
             </div>
 
             <p className="mt-10 mb-5 ml-1 text-2xl font-bold">Experience</p>
